a6b442239de2c8f08bd61166eb40309badb2d419
Josip Skako <josip.skako@maurer-electronics.hr>
2024-04-03 08:53:43 +0200

    add colours scheme

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,11 +1,16 @@
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import ui.theme.darkColorScheme
+import ui.theme.lightColorScheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) darkColorScheme else lightColorScheme
+    ) {
 
     }
 }
---
+++ composeApp/src/commonMain/kotlin/ui/theme/Theme.kt
@@ -0,0 +1,67 @@
+package ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+val lightColorScheme = lightColorScheme(
+    primary = Color(0xFF0036A5),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFFCCE4F0),
+    onPrimaryContainer = Color(0xFF001824),
+    inversePrimary = Color(0xFFFAE661),
+    secondary = Color(0xFF576164),
+    onSecondary = Color(0xFFF2F3F4),
+    secondaryContainer = Color(0xFFE5E8E9),
+    onSecondaryContainer = Color(0xFF000000),
+    tertiary = Color(0xFF006A3B),
+    onTertiary = Color(0xFF000000),
+    tertiaryContainer = Color(0xFFFDF1D8),
+    onTertiaryContainer = Color(0xFF000000),
+    background = Color(0xFFF2F3F4),
+    onBackground = Color(0xFF121212),
+    surface = Color(0xFFFAFBFC),
+    onSurface = Color(0xFF121212),
+    surfaceVariant = Color(0xFFCCDAE3),
+    onSurfaceVariant = Color(0xFF000000),
+    inverseSurface = Color(0xFF576164),
+    inverseOnSurface = Color(0xFFFFFFFF),
+    error = Color(0xFFBF003A),
+    onError = Color(0xFFFFFFFF),
+    errorContainer = Color(0xFFF2F3F4),
+    onErrorContainer = Color(0xFFCD5038),
+    outline = Color(0xFF9AA0A2),
+    outlineVariant = Color(0xFFDDDFE0),
+    surfaceContainer = Color(0xFFE2E2E5)
+)
+
+val darkColorScheme = darkColorScheme(
+    primary = Color(0xFF99B4EB),
+    onPrimary = Color(0xFF000029),
+    primaryContainer = Color(0xFF0077B6),
+    onPrimaryContainer = Color(0xFFFFFFFF),
+    inversePrimary = Color(0xFFFF8C00),
+    secondary = Color(0xFF66ADD3),
+    onSecondary = Color(0xFF000000),
+    secondaryContainer = Color(0xFF464E50),
+    onSecondaryContainer = Color(0xFFFFFFFF),
+    tertiary = Color(0xFF8EDEC6),
+    onTertiary = Color(0xFF000000),
+    tertiaryContainer = Color(0xFFD1A13C),
+    onTertiaryContainer = Color(0xFFFFFFFF),
+    background = Color(0xFF000000),
+    onBackground = Color(0xFFFFFFFF),
+    surface = Color(0xFF1C1B1F),
+    onSurface = Color(0xFFE6E1E5),
+    surfaceVariant = Color(0xFF2D2D2D),
+    onSurfaceVariant = Color(0xFFFFFFFF),
+    inverseSurface = Color(0xFF393939),
+    inverseOnSurface = Color(0xFFFFFFFF),
+    error = Color(0xFFD96688),
+    onError = Color(0xFF3B1214),
+    errorContainer = Color(0xFF2C2C2E),
+    onErrorContainer = Color(0xFFF08A8C),
+    outline = Color(0xFFEDEDED),
+    outlineVariant = Color(0xFF49454F),
+    surfaceContainer = Color(0xFF282A2D)
+)
